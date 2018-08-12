7545115f8f4996c59a153e0e4d8daad9b2fa5fa1
ghbhaha <ghbhaha@gmail.com>
2018-08-12 16:58:57 +0800

    代码优化

--- src/main/java/com/suda/MyVideoApi/service/VideoDiliServiceImpl.java
+++ src/main/java/com/suda/MyVideoApi/service/VideoDiliServiceImpl.java
@@ -1,8 +1,17 @@
 package com.suda.MyVideoApi.service;
 
 import com.suda.MyVideoApi.constant.API;
+import com.suda.MyVideoApi.util.JsoupUtils;
+import com.suda.MyVideoApi.util.SuplayerUtil;
+import org.jsoup.nodes.Document;
+import org.jsoup.select.Elements;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * @author guhaibo
@@ -18,5 +27,21 @@ public class VideoDiliServiceImpl extends BaseVideoService {
     @Override
     public API getApi() {
         return API.DILI;
+    }
+
+    @Override
+    protected String parsePlayUrl(String refererUrl) {
+        Document pcDocument = JsoupUtils.getDocWithPC(refererUrl);
+        Elements script = pcDocument.body().getElementsByTag("script");
+        String scriptText = script.get(1).data();
+        Pattern pattern = Pattern.compile("\"(.*?)\"");
+        Matcher m = pattern.matcher(scriptText);
+        List<String> params = new ArrayList<>();
+        while (m.find()) {
+            params.add(m.group().replace("\"", ""));
+        }
+        String playerUrl = "https://api.1suplayer.me/player/?userID=&type="
+                + params.get(0) + "&vkey=" + params.get(1);
+        return SuplayerUtil.getPlayUrl(refererUrl, playerUrl, "https://api.1suplayer.me/player/api.php");
     }
 }
