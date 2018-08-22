d66d9bcf177dc075ce280d4b155c9e0ff70bcb8d
ghbhaha <ghbhaha@gmail.com>
2018-08-22 12:34:47 +0800

    修复韩剧选集播放

--- src/main/java/com/suda/MyVideoApi/service/VideoHJServiceImpl.java
+++ src/main/java/com/suda/MyVideoApi/service/VideoHJServiceImpl.java
@@ -1,14 +1,17 @@
 package com.suda.MyVideoApi.service;
 
 import com.suda.MyVideoApi.constant.API;
+import com.suda.MyVideoApi.domian.dos.VideoSeriesDO;
 import com.suda.MyVideoApi.util.JsoupUtils;
 import com.suda.MyVideoApi.util.SuplayerUtil;
 import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -43,5 +46,25 @@ public class VideoHJServiceImpl extends BaseVideoService {
         String playerUrl = "https://api.1suplayer.me/player/?userID=&type="
                 + params.get(0) + "&vkey=" + params.get(1);
         return SuplayerUtil.getPlayUrl(refererUrl, playerUrl, "https://api.1suplayer.me/player/api.php");
+    }
+
+    @Override
+    protected List<VideoSeriesDO> parseVideoSeries(Document document) {
+        Elements serieses = document.getElementsByClass("video_list_li");
+        if (serieses == null) {
+            return Collections.emptyList();
+        }
+        List<VideoSeriesDO> videoSeriesDOS = new ArrayList<>();
+        if (serieses.size() > 0) {
+            for (Element seriese : serieses) {
+                for (Element element : seriese.getElementsByTag("a")) {
+                    VideoSeriesDO videoSeriesDO = new VideoSeriesDO();
+                    videoSeriesDO.setName(element.text());
+                    videoSeriesDO.setSeriesId(element.attr("href").replace("?Play-", ""));
+                    videoSeriesDOS.add(videoSeriesDO);
+                }
+            }
+        }
+        return videoSeriesDOS;
     }
 }
